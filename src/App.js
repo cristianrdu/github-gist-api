92c0ac21a9d41e3a7b07c815b374a86ea425330b
cristianrdu <chris54332@gmail.com>
2021-06-10 15:55:17 +0300

    added the Gists api fetching method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import './App.css';
+import React from 'react';
+import axios from "axios";
+
+const { useState } = React;
+
+  
+const fetchUserGistData = async (user) => {
+  return axios.get(`https://api.github.com/users/${user}/gists`)
+  .then(({data}) => {
+    return data;
+  }).catch( (error) => {
+    console.log(error);
+    return error;
+  });
+}
 
 function App() {
+  const [user, setUser] = useState('');
+  const [gistListData, setGistListData] = useState({});
+
+  const setUsername = (event) => {
+    setUser(event.target.value);
+  }
+
+  const fetchGistList = async event => {
+    event.preventDefault();
+
+    const data = await fetchUserGistData(user);
+    setGistListData(data || '');
+    console.log(data);
+  }
+
   return (
     <div className="App">
+
     </div>
   );
 }
